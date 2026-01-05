38f39f523b13097bbdc772e792c6f2ad7c17b343
AnhP <157474368+PhamHuynhAnh16@users.noreply.github.com>
2026-01-05 20:39:32 +0700

    fix

--- main/app/core/ui.py
+++ main/app/core/ui.py
@@ -429,7 +429,7 @@ def change_embedders_mode(value):
 def change_fp(fp):
     fp16 = fp == "fp16"
 
-    if fp16 and config.allow_is_half: 
+    if fp16 and not config.allow_is_half: 
         gr_warning(translations["fp16_not_support"])
         return "fp32"
     else:
